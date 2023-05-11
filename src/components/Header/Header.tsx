5c4f56f47ce45987182b3021bf35f9342bb80357
rayhan kabir <rayhan.kabir29@gmail.com>
2023-05-11 16:45:59 +0600

    header css

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.style.css";
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h2>React : Simple CRUD Application</h2>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
